3d1ba20b73b04dc151bb3c2809155e5d7fa85efb
U-AVWORLD\dale8774 <dale8774@DALERA.esri.com>
2016-05-26 17:18:00 -0700

    initial tries to retrieve countries list

--- js/main.js
+++ js/main.js
@@ -7,7 +7,15 @@ angular
       })
       .when('/radiomap', {
         templateUrl: '/templates/radiomap.html',
-        controller: 'RadioMapCtrl'
+        controller: 'RadioMapCtrl',
+        controllerAs: 'vm',
+        resolve: {
+          topCountryArtists: function($route, $http, $location) {
+          
+            //http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=spain&api_key=b618453443ef733c126b9d88855f9702&format=json
+            //http://download.geonames.org/export/dump/countryInfo.txt
+          }
+        }
       })
       .when('/fanmap', {
         templateUrl: '/templates/fanmap.html',

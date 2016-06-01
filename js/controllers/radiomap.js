c84ca521c7fd37d4ba399199816f7baf6860e491
DalerAsrorov <asrorids@gmail.com>
2016-06-01 16:44:44 -0700

    adding graphics is now possible

--- js/controllers/radiomap.js
+++ js/controllers/radiomap.js
@@ -5,6 +5,8 @@ angular
     var cont = [];
     vm.text = "Some random text";
 
+    console.log('Radio Map');
+
     var extractPromises = function(i) {
       var p = Promise.resolve(topCountryArtists[i]);
       p.then(function(data) {
@@ -21,21 +23,81 @@ angular
     // display map on the page
     function displayMap() {
       require([
-        "esri/Map",
-        "esri/views/SceneView",
-        "dojo/domReady!"
-      ], function(Map, SceneView) {
-        var map = new Map({
-            basemap: "streets",
-            ground: "world-elevation"
-        });
-        var view = new SceneView({
-          container: "map",  // Reference to the DOM node that will contain the view
-          map: map  // References the map object created in step 3
-        });
+           "esri/Map",
+           "esri/PopupTemplate",
+           "esri/views/MapView",
+           "esri/Graphic",
+           "esri/geometry/Point",
+           "esri/geometry/Polyline",
+           "esri/geometry/Polygon",
+           "esri/symbols/SimpleMarkerSymbol",
+           "esri/symbols/SimpleLineSymbol",
+           "esri/symbols/SimpleFillSymbol",
+           "dojo/domReady!"
+         ], function(
+           Map, PopupTemplate, MapView,
+           Graphic, Point, Polyline, Polygon,
+           SimpleMarkerSymbol, SimpleLineSymbol, SimpleFillSymbol
+         ) {
+
+           var map = new Map({
+             basemap: "gray"
+           });
+
+           var view = new MapView({
+             center: [-80, 35],
+             container: "map",
+             map: map,
+             zoom: 3
+           });
 
 
-      });
+
+           var points = [
+
+           ]
+
+           /**********************
+            * Create a point graphic
+            **********************/
+
+           // Create an object for storing attributes related to the line
+           var lineAtt = {
+             Name: "Keystone Pipeline",
+             Owner: "TransCanada",
+             Length: "3,456 km"
+           };
+
+           // First create a point geometry (this is the location of the Titanic)
+           var point = new Point({
+             longitude: -49.97,
+             latitude: 41.73
+           });
+
+           // Create a symbol for drawing the point
+           var markerSymbol = new SimpleMarkerSymbol({
+             color: [226, 119, 40],
+             outline: { // autocasts as new SimpleLineSymbol()
+               color: [255, 255, 255],
+               width: 4
+             }
+           });
+
+           // Create a graphic and add the geometry and symbol to it
+           var pointGraphic = new Graphic({
+             geometry: point,
+             symbol: markerSymbol,
+             attributes: lineAtt,
+             popupTemplate: new PopupTemplate({
+               title: "{Name}",
+               content: "{*}"
+             })
+           });
+
+
+           // Add the graphics to the view's graphics layer
+           view.graphics.addMany([pointGraphic, polylineGraphic, polygonGraphic]);
+         });
 
     }
 
